b30250d6d35add4c879267e1681a5ec83b152fc5
OneHikaro <oleg81_404@mail.ru>
2024-02-22 21:19:21 +0300

    8

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,16 @@
 import './style.scss'
-console.log('777')
+let num = 5
+if (num > 0) {
+  console.log('1')
+}
+else {
+  if (num < 0) {
+    console.log ('-1')
+  }
+  else {
+    console.log('0')}
+}
+
+
+
+let result =  (1 + 1 > 4) ? 'Мало' : 'Много'
