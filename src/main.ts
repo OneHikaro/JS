47dcbb187792a5fa7d64eae3334da369b07d637f
OneHikaro <oleg81_404@mail.ru>
2024-02-27 21:19:12 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import './style.scss'
-let name = prompt('Введите свое имя')
-console.log('Привет,', name,'!')
+// Задание 1
+// let name = prompt('Введите свое имя')
+// console.log('Привет,', name,'!')
+
+// Задание 2
+const thisYear = 2024
+let YearOfBorn = 1981
+let yearsOld = thisYear-YearOfBorn
+console.log('Вам', yearsOld, 'лет (года).')
