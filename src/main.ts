2de2bea5c793bf66e5d7b75358eb700d322305e8
OneHikaro <oleg81_404@mail.ru>
2024-02-27 21:06:57 +0300

    27.02.2024TOP

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import './style.scss'
-alert ('Введиет свое имя')
-// let name = console.log (alert('Введите свое имя'))
-// console.log('Привет', name)
+let name = prompt('Введите свое имя')
+console.log('Привет,', name,'!')
