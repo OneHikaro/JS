a1e9c205e55d3de272d248e834e392c0c9f309aa
OneHikaro <oleg81_404@mail.ru>
2024-02-27 20:55:42 +0300

    27/02/2024TOP

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,4 @@
 import './style.scss'
-let num = 5
-if (num > 0) {
-  console.log('1')
-}
-else {
-  if (num < 0) {
-    console.log ('-1')
-  }
-  else {
-    console.log('0')}
-}
-
-
-
-let result =  (1 + 1 > 4) ? 'Мало' : 'Много'
+alert ('Введиет свое имя')
+// let name = console.log (alert('Введите свое имя'))
+// console.log('Привет', name)
