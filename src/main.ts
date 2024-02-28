45fdeb1f086cb5fde115c0a13a29455ec948d909
OneHikaro <oleg81_404@mail.ru>
2024-02-28 21:10:56 +0300

    28.02.2024HOME

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,21 @@ import './style.scss'
 // console.log('Привет,', name,'!')
 
 // Задание 2
-const thisYear = 2024
-let YearOfBorn = 1981
-let yearsOld = thisYear-YearOfBorn
-console.log('Вам', yearsOld, 'лет (года).')
+// const thisYear = 2024
+// let YearOfBorn = 1981
+// let yearsOld = thisYear-YearOfBorn
+// console.log('Вам', yearsOld, 'лет (года).')
+
+// Задание 3
+// let sideOfSquare = 6
+// console.log('Периметр квадрата', sideOfSquare*4)
+
+// Задание 4
+// const pi = 3.14
+// let r = 8
+// console.log(pi*r*r)
+
+// Задание 5
+let distance = 80
+let time = 2
+console.log(distance/time)
